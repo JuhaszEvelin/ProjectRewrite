c8512db7d4dcb14a71f1496a82291881a9414ed5
Juhasz Evelin <juhaaszevelin@gmail.com>
2021-04-19 10:32:56 +0200

    hashTable implemented generic type

--- src/main/java/hashtable/HashTable.java
+++ src/main/java/hashtable/HashTable.java
@@ -1,51 +1,113 @@
 package hashtable;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
-public class HashTable {
+public class HashTable<K, V> {
 
-    // Number of all buckets - Do not modify!
     private final int bucketsSize = 16;
 
-    private List<List<Entry>> buckets;
+    private List<List<Entry>> buckets = new ArrayList<>(bucketsSize);
 
-    private int getBucketIndexForKey(String key) {
-        throw new RuntimeException("FIXME");
+    public HashTable() {
+        for (int i = 0; i < bucketsSize; i++) {
+            buckets.add(new LinkedList<Entry>());
+        }
+    }
+
+    private int getBucketIndexForKey(K key) {
+
+        if (key == null) {
+            return 0;
+        }
+        int bucketIndex = Math.abs(key.hashCode() % bucketsSize);
+
+        return bucketIndex;
     }
 
     private List<Entry> getBucketAtIndex(int position) {
-        throw new RuntimeException("FIXME");
+        return buckets.size() > position ? buckets.get(position) : new LinkedList<Entry>();
     }
 
-    private Entry findEntryInBucket(String key, List<Entry> bucket) {
-        throw new RuntimeException("FIXME");
+    private Entry findEntryInBucket(K key, List<Entry> bucket) {
+        Entry entryRes = new Entry(key, null);
+        if (!bucket.isEmpty()) {
+
+            for (Entry entry : bucket) {
+                if (entry.key == null && key == null
+                        || entry.key.equals(key)) {
+                    entryRes = entry;
+                }
+            }
+        }
+        return entryRes;
     }
 
-    public Integer get(String key) {
-        throw new RuntimeException("FIXME");
+    public V get(K key) {
+        List<Entry> bucket = getBucketAtIndex(getBucketIndexForKey(key));
+        Entry entry = findEntryInBucket(key, bucket);
+        return entry == null ? null : (V) entry.getValue();
     }
 
-    public void put(String key, Integer value) {
-        throw new RuntimeException("FIXME");
+    public void put(K key, V value) {
+        List<Entry> bucket = new LinkedList<>();
+        int bucketIndexForKey = getBucketIndexForKey(key);
+        if (!buckets.isEmpty()
+                && bucketIndexForKey < buckets.size()
+                && !buckets.get(bucketIndexForKey).isEmpty()) {
+            bucket = getBucketAtIndex(bucketIndexForKey);
+            V oldValue = get(key);
+
+            if (oldValue == null) {
+                bucket.add(new Entry(key, value));
+                return;
+            } else {
+                findEntryInBucket(key, bucket).setValue(value);
+                return;
+            }
+        }
+        bucket.add(new Entry(key, value));
+        buckets.add(bucketIndexForKey, bucket);
     }
 
-    public Integer remove(String key) {
-        throw new RuntimeException("FIXME");
+    public V remove(K key) {
+        List<Entry> bucket = getBucketAtIndex(getBucketIndexForKey(key));
+        Entry entry = findEntryInBucket(key, bucket);
+        V result = (V) entry.getValue();
+        bucket.remove(entry);
+        return result;
     }
 
     public void clear() {
-        throw new RuntimeException("FIXME");
+        buckets.clear();
     }
 }
 
-class Entry {
+class Entry<K, V> {
 
-    public String key;
-    public Integer value;
+    public K key;
+    public V value;
 
-    public Entry(String key, Integer value) {
+    public Entry(K key, V value) {
         this.key = key;
         this.value = value;
     }
 
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
 }
+
